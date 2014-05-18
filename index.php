c9bd7ddb39ea4d1b816df3fddbd8f04dea97cdc3
Ridwan S. Hendharto <one.hendharto@gmail.com>
2014-05-18 05:42:47 -0700

    Update index.php
    edit script

--- index.php
+++ index.php
@@ -29,15 +29,8 @@
 					</form>
 					<h1><a href="index.php">PROJECT TUBES 1</a></h1>
 					<?php
-					// we can specify a localy version or a remote xml as in example
-					$xml = simplexml_load_file("http://rss.detik.com/index.php/otomotif");
-					// if I use a local file saved in /tmp directory: $xml = simplexml_load_file("/tmp/podcast.xml");
- 
-					// now make a XPath selection: select all <item> tag.
+					$xml = simplexml_load_file("http://rss.detik.com/index.php/otomotif")
 					$items = $xml->xpath("/rss/channel/item");
- 
-					// Now, $items contains a class version of a <item> tag.
-					// So, if I'd like to print only the title of news item:
 					foreach($items as $item){
 					echo $item->title.'\n';
 					}
